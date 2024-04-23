91b859a658c63fde46a33f3d6d46f8457aaabcbc
gr4per <arne.graeper@eon.com>
2024-04-23 09:14:42 +0200

    improving install docs for hardware

--- remote_instr.sh
+++ remote_instr.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 if [ -z ${SSH_PORT+x} ]; then echo "please export SSH_PORT to be configured on raspberry pi before using the script"; exit 1; else echo "raspberry pi will be configured to use port '$SSH_PORT' for ssh"; fi
+if [ -z ${RASPI_HOSTNAME+x} ]; then echo "please export RASPI_HOSTNAME to be configured on raspberry pi before using the script"; exit 1; else echo "raspberry pi will be connected via hoastname '$RASPI_HOSTNAME'"; fi
 
 # assuming ubuntu server 20.04.3 LTS image
 # be aware ubuntu requires manual ssh login with initial passwd change
-export RASPI_HOSTNAME=ubuntu
 export RASPI_USER=ubuntu
-export SSH_PORT=58736
 
 # get ip
 RASPI_IP=$(ping $RASPI_HOSTNAME -n 1 | grep -E "Antwort von (.*?):" | sed -r 's/Antwort von (.*?):.*/\1/g')
+echo "RASPI IP = " + $RASPI_IP
 
 # transfer pub key to raspi
 scp $RASPI_PUB_KEY $RASPI_USER@$RASPI_HOSTNAME:/home/$RASPI_USER/public_keyfile
+echo "PUB KEY transferred to RASPI"
 
 # connect 1st ssh
 ssh $RASPI_USER@$RASPI_HOSTNAME << ENDSSH
